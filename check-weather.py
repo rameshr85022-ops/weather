d04482675dea26f67922175dc2c54a0d72bddf07
RAMESH <rameshr85022@gmail.com>
2026-09-18 14:10:07 +0530

    Checked the weather

---
+++ check-weather.py
@@ -0,0 +1,8 @@
+temperature = float(input("Enter temperature in °C: "))
+
+if temperature >= 30:
+    print("Weather is Hot")
+elif temperature <= 20:
+    print("Weather is Cold")
+else:
+    print("Weather is Normal")
